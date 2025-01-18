de682186b361687fb658fe553b77e4b2dce6b93f
Anthony Daves <adaves1@yahoo.com>
2025-01-18 19:35:33 +0530

    create iconv-lite.d.ts

---
+++ src/typings/iconv-lite.d.ts
@@ -0,0 +1,13 @@
+
+
+declare module 'iconv-lite' {
+	export function decode(buffer: NodeBuffer, encoding:string, options?:any): string;
+
+	export function encode(content:string, encoding:string, options?:any): NodeBuffer;
+
+	export function encodingExists(encoding:string): boolean;
+
+	export function decodeStream(encoding:string): NodeJS.EventEmitter;
+
+	export function encodeStream(encoding:string): NodeJS.EventEmitter;
+}
